6a0872903f353893d7eb2cf73212743f40886d9c
gohopo <gohopo@live.com>
2024-11-01 15:41:30 +0800

    chore:适配3.24.4

--- android/src/main/java/com/gohopo/social_foundation_em/SocialFoundationEmPlugin.java
+++ android/src/main/java/com/gohopo/social_foundation_em/SocialFoundationEmPlugin.java
@@ -2,7 +2,6 @@ package com.gohopo.social_foundation_em;
 
 import android.app.Activity;
 import android.content.Context;
-
 import androidx.annotation.NonNull;
 
 import com.gohopo.social_foundation_em.push.PushManager;
@@ -15,15 +14,10 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** SocialFoundationEmPlugin */
 public class SocialFoundationEmPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
-  private Context context;
-  private Activity activity;
+  public static MethodChannel channel;
+  public static Context context;
+  public static Activity activity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
