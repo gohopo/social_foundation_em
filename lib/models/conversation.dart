477c08a615d003b2c67ba0a4e5eddaed335e9671
gohopo <gohopo@live.com>
2026-01-21 10:27:44 +0800

    refactor(im): 改造

--- lib/models/conversation.dart
+++ lib/models/conversation.dart
@@ -2,10 +2,4 @@ import 'package:social_foundation/social_foundation.dart';
 
 abstract class SfConversationEm<TMessage extends SfMessage> extends SfConversation<TMessage>{
   SfConversationEm(super.data);
-  @override
-  Future read() async {
-    if(unreadMessagesCount==0) return;
-    SfLocatorManager.chatManager.convRead(convId);
-    return super.read();
-  }
 }
--- lib/services/chat_manager.dart
+++ lib/services/chat_manager.dart
@@ -2,8 +2,9 @@ import 'dart:convert';
 
 import 'package:im_flutter_sdk/im_flutter_sdk.dart';
 import 'package:social_foundation/social_foundation.dart';
+import 'package:social_foundation_em/models/conversation.dart';
 
-abstract class SfChatManagerEm<TConversation extends SfConversation,TMessage extends SfMessage> extends SfChatManager<TConversation,TMessage>{
+abstract class SfChatManagerEm<TConversation extends SfConversationEm,TMessage extends SfMessage> extends SfChatManager<TConversation,TMessage>{
   String get appKey;
   Map<String, String>? get extSettings => null;
   EMOptions get options => EMOptions.withAppKey(
@@ -21,9 +22,10 @@ abstract class SfChatManagerEm<TConversation extends SfConversation,TMessage ext
   @override
   Future convQuit(String conversationId) => EMClient.getInstance.chatRoomManager.leaveChatRoom(conversationId);
   @override
-  Future convRead(String conversationId) async {
-    var conversation = await protectedGetConversation(conversationId);
-    return conversation.markAllMessagesAsRead();
+  Future convRead(TConversation conversation) async {
+    var convType = conversation.type==0 ? EMConversationType.Chat : conversation.type==1 ? EMConversationType.GroupChat : EMConversationType.ChatRoom;
+    var emConversation = await protectedGetConversation(conversation.convId,type:convType);
+    return emConversation.markAllMessagesAsRead();
   }
   @override
   Future convRecall(String messageID,{String? conversationId,int? timestamp}) => EMClient.getInstance.chatManager.recallMessage(messageID);
@@ -61,38 +63,39 @@ abstract class SfChatManagerEm<TConversation extends SfConversation,TMessage ext
   }
   @override
   Future login(String userId,{String? token}) => EMClient.getInstance.loginWithToken(userId,token!);
-  @override
-  void onMessageReceived(TMessage message) async {
-    if(!message.transient) await updateUnreadMessagesCount(message.convId);
-    super.onMessageReceived(message);
-  }
-  Future<TConversation> protectedConvertConversation(EMConversation conversation) async {
-    var lastMessage = await conversation.latestMessage();
-    var map = {};
-    map['ownerId'] = SfLocatorManager.userState.curUserId;
-    map['convId'] = conversation.id;
-    map['name'] = 'chat';
-    map['creator'] = SfLocatorManager.userState.curUserId;
-    map['members'] = conversation.type==EMConversationType.Chat?[SfLocatorManager.userState.curUserId,conversation.id]:[];
-    map['unreadMessagesCount'] = await conversation.unreadCount();
-    map['lastMessage'] = protectedConvertMessage(lastMessage);
-    map['lastMessageAt'] = lastMessage?.serverTime;
+  TConversation protectedConvertConversation(EMConversation conversation){
+    Map map = {
+      'convId': conversation.id,
+      'dict': {
+        '__type': conversation.type==EMConversationType.Chat ? 0 : conversation.type==EMConversationType.GroupChat ? 1 : 2
+      },
+      'members': conversation.type==EMConversationType.Chat ? [SfLocatorManager.userState.curUserId,conversation.id] : []
+    };
     return conversationFactory(map);
   }
-  TMessage? protectedConvertMessage(EMMessage? message){
-    if(message==null) return null;
-    var map = {};
-    map['ownerId'] = SfLocatorManager.userState.curUserId;
-    map['msgId'] = message.msgId;
-    map['convId'] = message.conversationId;
-    map['fromId'] = message.from;
-    map['timestamp'] = message.serverTime;
-    map['status'] = protectedConvertStatus(message.status);
-    map['receiptTimestamp'] = message.hasDeliverAck ? DateTime.now().millisecondsSinceEpoch : null;
+  TMessage protectedConvertMessage(EMMessage message){
+    var map = {
+      'convId': message.conversationId,
+      'fromId': message.from,
+      'msgId': message.msgId,
+      'receiptTimestamp': message.hasDeliverAck ? DateTime.now().millisecondsSinceEpoch : null,
+      'status': protectedConvertStatus(message.status),
+      'timestamp': message.serverTime,
+    };
     if(message.body.type == MessageType.TXT){
       map.addAll(jsonDecode((message.body as EMTextMessageBody).content));
     }
-    return messageFactory(map);
+    var data = messageFactory(map);
+
+    if(message.chatType == ChatType.Chat){
+      if(data.msgExtra['__cid']!=null) data.convId = data.msgExtra['__cid'];
+    }
+    else if(message.chatType == ChatType.ChatRoom){
+      data.msgExtra['transient'] = true;
+    }
+    data.msgExtra['transient'] ??= message.deliverOnlineOnly;
+
+    return data;
   }
   int protectedConvertStatus(MessageStatus status){
     switch(status){
@@ -116,12 +119,19 @@ abstract class SfChatManagerEm<TConversation extends SfConversation,TMessage ext
   }
   void protectedOnMessagesRecalledInfo(List<RecallMessageInfo> messages){
     for(var message in messages){
-      onMessageRecalled(protectedConvertMessage(message.recallMessage)!);
+      if(message.recallMessage==null) continue;
+      onMessageRecalled(protectedConvertMessage(message.recallMessage!));
     }
   }
   void protectedOnMessagesReceived(List<EMMessage> messages){
-    for(var message in messages){
-      onMessageReceived(protectedConvertMessage(message)!);
+    var list = messages
+      .map((x) => protectedConvertMessage(x))
+      .sorted((a,b) => a.timestamp.compareTo(b.timestamp));
+
+    protectedUnreadMessages(list.where((x) => !x.transient));
+
+    for(var message in list){
+      onMessageReceived(message);
     }
   }
   Future<EMMessage> protectedSend(EMMessage message,bool transient){
@@ -129,24 +139,56 @@ abstract class SfChatManagerEm<TConversation extends SfConversation,TMessage ext
     return EMClient.getInstance.chatManager.sendMessage(message);
   }
   @override
-  Future<TMessage> protectedSendMessage(String conversationId,String? msg,String msgType,Map msgExtra) async {
+  Future<TMessage> protectedSendMessage(TConversation conversation,String? msg,String msgType,Map msgExtra) async {
+    var chatType = conversation.type==0 ? ChatType.Chat : conversation.type==1 ? ChatType.GroupChat : ChatType.ChatRoom;
+    var conversationId = conversation.convId;
+    if(chatType==ChatType.Chat && conversation.otherId!=conversationId){
+      msgExtra['__cid'] = conversationId;
+      msgExtra['__cn'] = conversation.name;
+      conversationId = conversation.otherId!;
+    }
+
     var message = EMMessage.createTxtSendMessage(
       targetId: conversationId,
       content: jsonEncode({
         'msg': msg,
         'msgType': msgType,
         'msgExtra': msgExtra
-      })
+      }),
+      chatType: chatType
     );
     var result = await protectedSend(message,msgExtra['transient']??false);
-    return protectedConvertMessage(result)!;
+    return protectedConvertMessage(result);
   }
-  Future updateUnreadMessagesCount(String conversationId) async {
-    var emConversation = await getConversation(conversationId);
-    var conversation = (await SfLocatorManager.chatState.queryConversation(conversationId)) as TConversation? ?? emConversation;
-    conversation.unreadMessagesCount = emConversation.unreadMessagesCount;
-    conversation.lastMessage = emConversation.lastMessage;
-    conversation.lastMessageAt = emConversation.lastMessageAt;
-    saveConversation(conversation);
+  void protectedUnreadMessage(String conversationId,Iterable<TMessage> messages) async {
+    var lastMessage = messages.last;
+    var conversation = SfLocatorManager.chatState.getConversation(conversationId) ?? conversationFactory(
+      lastMessage.msgExtra['__cid']!=null || lastMessage.convId==lastMessage.fromId ? {
+        'convId': lastMessage.msgExtra['__cid'] ?? lastMessage.convId,
+        'dict': {'__type':0},
+        'members': [SfLocatorManager.userState.curUserId,lastMessage.fromId],
+        'name': lastMessage.msgExtra['__cn']
+      } : {
+        'convId': lastMessage.convId,
+        'dict': {'__type':1},
+      }
+    );
+    conversation.lastMessage = lastMessage;
+    conversation.lastMessageAt = lastMessage.timestamp;
+    conversation.unreadMessagesCount += messages.length;
+    saveConversation(conversation as TConversation);
+    convRead(conversation);
+    SfUnreadMessagesCountUpdatedEvent<TConversation>(conversation:conversation).emit();
+  }
+  void protectedUnreadMessages(Iterable<TMessage> messages){
+    var map = messages.fold<Map<String,List<TMessage>>>({},(t,x){
+      t[x.convId] ??= [];
+      t[x.convId]?.add(x);
+      return t;
+    });
+
+    for(var item in map.entries){
+      protectedUnreadMessage(item.key,item.value);
+    }
   }
 }
